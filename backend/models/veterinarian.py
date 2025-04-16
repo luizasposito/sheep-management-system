bab7cd94e2f181d37ef2718a1c8785bcc378a3b4
luiza <luizarochasposito@gmail.com>
2025-04-17 00:05:28 +0100

    added put veterinarian endpoint

---
+++ backend/models/veterinarian.py
@@ -0,0 +1,13 @@
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class Veterinarian(Base):
+    __tablename__ = "veterinarian"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    farm_id = Column(Integer, ForeignKey("farm.id"), nullable=False)
+    farmer_id = Column(Integer, ForeignKey("farmer.id"), nullable=False)
